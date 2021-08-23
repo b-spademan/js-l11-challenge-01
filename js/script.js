0eb959c6f4fdee1340afec72c4a6afe79c86644b
Brenna Spademan <bspademan87@gmail.com>
2021-08-23 09:19:43 -0400

    functions added

--- js/script.js
+++ js/script.js
@@ -3,9 +3,34 @@ const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
 
+// API set up to pull random images
 const getImage = async function () {
     const res = await fetch ("https://picsum.photos/v2/list?limit=100");
     const images = await res.json();
-    console.log(images);
+    // console.log(images);
+    selectRandomImage(images);
 };
 getImage();
+
+// Function to randomly select an image from API
+const selectRandomImage = function (images) {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    // console.log(randomIndex);
+    const randomImage = images[randomIndex];
+    // console.log(randomImage);
+    displayImage(randomImage);
+};
+
+// Function to display image
+const displayImage = function (randomImage) {
+    const author = randomImage.author;
+    const imageAddress = randomImage.download_url;
+    authorSpan.innerText = author;
+    img.src = imageAddress;
+    imgDiv.classList.remove("hide");
+};
+
+// Click event on button 
+button.addEventListener("click", function() {
+    getImage();
+});
